f1c3959e00dec1fff85fae5dc4ae9a2cb259e9a6
Francis <hisconer@gmail.com>
2015-12-28 19:15:32 +0100

    Cambio en estructura de carpetas

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( "1" === "1", "Passed!" );
+});
+
+QUnit.test2( "hello test 2", function( assert ) {
+  assert.ok( typeof("jandler") == string, "" );
+});
+
+QUnit.test( "move return translate3d", function(assert){
+  // code
+});
+
+
