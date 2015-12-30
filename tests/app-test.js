a94b86d352cc36eb9b52c1504385efa3d118120b
Francis <hisconer@gmail.com>
2015-12-30 19:31:38 +0100

    Add phantomjs

---
+++ tests/app-test.js
@@ -0,0 +1,10 @@
+describe("App", function() {
+
+  it("should set the background color", function() {
+
+    window.scrollTop(0, 0);
+    console.log($('.box__shape--one').css("opacity"));
+    expect(true).to.be.true;
+  })
+
+})
--- tests/tests.js
+++ tests/tests.js
@@ -1,89 +1,84 @@
 var expect = chai.expect;
+var assert = chai.assert;
 
 describe("Smowl.js unit tests", function() {
 
-  describe("Constructor", function() {
-    it("Can access to smowl about.autor property", function() {
-      expect($$().typename).to.equal("smowl");
-    });
+/*
+ *  describe("Constructor", function() {
+ *    it("Can access to smowl about.autor property", function() {
+ *      expect($$().typename).to.equal("smowl");
+ *    });
+ *  });
+ *
+ *  describe("Plugin Access", function() {
+ *    it("Can access to fadeIn()", function() {
+ *      expect(typeof($$().fadeIn())).equal('object');
+ *    });
+ *    it("Can access to fadeOut()", function() {
+ *      expect(typeof($$().fadeIn())).equal('object');
+ *    });
+ *    it("Can access to modParallax()", function() {
+ *      expect(typeof($$().modParallax())).equal('object');
+ *    });
+ *    it("Can access to heroParallax()", function() {
+ *      expect(typeof($$().heroParallax())).equal('object');
+ *    });
+ *    it("Can access to spriteAnim()", function() {
+ *      expect(typeof($$().spriteAnim())).equal('object');
+ *    });
+ *  });
+ *
+ *  describe("Core methods", function() {
+ *    it("Se puede asignar la funcion core soa() al elemento y devuelve un objeto smowl", function() {
+ *      var soa = $$('.box__shape--one').soa(
+ *          { "from":{}, "to":{} }, "100%", 0, 'onEnter', 1, true, false
+ *      );
+ *      expect(soa.typename).equal('smowl');
+ *    });
+ *  });
+ */
+
+/*
+ *  describe("Smowl Global vars", function() {
+ *    it("Se puede asignar la funcion core soa() al elemento y devuelve un objeto smowl", function() {
+ *
+ *      // Changue duration, for example
+ *      window.smowlDuration = '70%';
+ *
+ *      // Empty init
+ *      $$();
+ *
+ *      // Test global configuration
+ *      expect(typeof($$) == 'function').to.be.true;
+ *      expect(window.smowl_setup.duration  === '70%').to.be.true;
+ *      expect(window.smowl_setup.offset  === 0).to.be.true;
+ *      expect(window.smowl_setup.triggerHook === 'onEnter').to.be.true;
+ *      expect(window.smowl_setup.time  === 1).to.be.true;
+ *      expect(window.smowl_setup.reverse  === true).to.be.true;
+ *      expect(window.smowl_setup.ease  === "Power0.easeNone").to.be.true;
+ *      expect(window.smowl_setup.delay  === 0).to.be.true;
+ *      expect(window.smowl_setup.frameDir === "x").to.be.true;
+ *      expect(window.smowl_setup.indicators === false).to.be.true;
+ *      expect(window.smowl_setup.console === false).to.be.true;
+ *
+ *    });
+ *  })
+ */
+
+  describe("Testing fadeIn() in .box__shape--one", function() {
+
+
+    it("1 debe ser igual que 1", function() {
+      window.scrollTo(0,299);
+      $$(".box__shape--one").fadeIn();
+      console.log($(".box__shape--one").prop("style")["opacity"]);
+      console.log($(".box__shape--one").css("opacity"));
+    })
+
   });
 
-  describe("Plugin Access", function() {
-    it("Can access to fadeIn()", function() {
-      expect(typeof($$().fadeIn())).equal('object');
-    });
-    it("Can access to fadeOut()", function() {
-      expect(typeof($$().fadeIn())).equal('object');
-    });
-    it("Can access to modParallax()", function() {
-      expect(typeof($$().modParallax())).equal('object');
-    });
-    it("Can access to heroParallax()", function() {
-      expect(typeof($$().heroParallax())).equal('object');
-    });
-    it("Can access to spriteAnim()", function() {
-      expect(typeof($$().spriteAnim())).equal('object');
-    });
-  });
-
-  describe("Core methods", function() {
-    it("Se puede asignar la funcion core soa() al elemento y devuelve un objeto smowl", function() {
-      var soa = $$('.box__shape--one').soa(
-          { "from":{}, "to":{} }, "100%", 0, 'onEnter', 1, true, false
-      );
-      expect(soa.typename).equal('smowl');
-    });
-  });
-
-  describe("Smowl Global vars", function() {
-    it("Se puede asignar la funcion core soa() al elemento y devuelve un objeto smowl", function() {
-
-      // Changue duration, for example
-      window.smowlDuration = '70%';
-
-      // Empty init
-      $$();
-
-      // Test global configuration
-      expect(typeof($$) == 'function').to.be.true;
-      expect(window.smowl_setup.duration  === '70%').to.be.true;
-      expect(window.smowl_setup.offset  === 0).to.be.true;
-      expect(window.smowl_setup.triggerHook === 'onEnter').to.be.true;
-      expect(window.smowl_setup.time  === 1).to.be.true;
-      expect(window.smowl_setup.reverse  === true).to.be.true;
-      expect(window.smowl_setup.ease  === "Power0.easeNone").to.be.true;
-      expect(window.smowl_setup.delay  === 0).to.be.true;
-      expect(window.smowl_setup.frameDir === "x").to.be.true;
-      expect(window.smowl_setup.indicators === false).to.be.true;
-      expect(window.smowl_setup.console === false).to.be.true;
-
-    });
-  });
 });
 /*
-QUnit.test( "Global setup tests", function( assert ) {
-
-  // Changue duration, for example
-  window.smowlDuration = '70%';
-
-  // Empty init
-  $$();
-
-  // Test global configuration
-  assert.ok( typeof($$) === 'function', "Passed!");
-  assert.ok( window.smowl_setup.duration    === '70%',             "Smowl duration Passed!");
-  assert.ok( window.smowl_setup.offset      === 0,                 "Smowl offset Passed!");
-  assert.ok( window.smowl_setup.triggerHook === 'onEnter',         "Smowl triggerHook Passed!");
-  assert.ok( window.smowl_setup.time        === 1,                 "Smowl time Passed!");
-  assert.ok( window.smowl_setup.reverse     === true,              "Smowl reverse Passed!");
-  assert.ok( window.smowl_setup.ease        === "Power0.easeNone", "Smowl triggerHook Passed!");
-  assert.ok( window.smowl_setup.delay       === 0,                 "Smowl delay Passed!");
-  assert.ok( window.smowl_setup.frameDir    === "x",               "Smowl frameDir Passed!");
-  assert.ok( window.smowl_setup.indicators  === false,             "Smowl indicators Passed!");
-  assert.ok( window.smowl_setup.console     === false,             "Smowl console Passed!");
-
-});
-
 QUnit.test( "fadeIn() Plugin tests", function( assert ) {
 
   // fadein()
